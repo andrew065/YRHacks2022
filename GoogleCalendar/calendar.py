4ae377f0bd9bd38b11c76c8f594709044ee464e2
andrew065 <andrewlian345@gmail.com>
2022-04-08 21:07:58 -0400

    calendar.py

---
+++ GoogleCalendar/calendar.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+all_events = []
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def main():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('../credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    service = build('calendar', 'v3', credentials=creds)
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    events = service.events().list(calendarId='primary', timeMin=now, singleEvents=True, orderBy='startTime').execute()
+
+    if not events:
+        print('No upcoming events found.')
+        return
+
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+
+if __name__ == '__main__':
+    main()
--- bot.py
+++ bot.py
@@ -6,4 +6,10 @@ bot = commands.Bot(command_prefix='!')
 bot.load_extension('somecommands')
 bot.load_extension('googlecalendar')
 
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+
+bot.load_extension("somecommands")
+
 bot.run(token)
